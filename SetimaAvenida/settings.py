38564bee59b6bc643e5e505a3e15116b5f9206e7
skapskap <skaprange@gmail.com>
2023-05-07 15:52:29 -0300

    set up postgresql

--- SetimaAvenida/settings.py
+++ SetimaAvenida/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'SetimaAvenida.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'setimaavenidadb',
+        'USER': 'postgres',
+        'PASSWORD': 'aptx4869',
+        'HOST': 'localhost',
     }
 }
 
