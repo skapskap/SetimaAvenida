f817f85fdeecae705e870c32e0e8055c8acf2a85
skapskap <skaprange@gmail.com>
2023-05-10 15:36:23 -0300

    built base search pages

---
+++ listings/choices.py
@@ -0,0 +1,55 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': 'R$100,000',
+    '200000': 'R$200,000',
+    '300000': 'R$300,000',
+    '400000': 'R$400,000',
+    '500000': 'R$500,000',
+    '600000': 'R$600,000',
+    '700000': 'R$700,000',
+    '800000': 'R$800,000',
+    '900000': 'R$900,000',
+    '1000000': 'R$1M+',
+}
+
+state_choices = {
+'AC': 'Acre',
+'AL': 'Alagoas',
+'AP': 'Amapá',
+'AM': 'Amazonas',
+'BA': 'Bahia',
+'CE': 'Ceará',
+'DF': 'Distrito Federal',
+'ES': 'Espírito Santo',
+'GO': 'Goiás',
+'MA': 'Maranhão',
+'MT': 'Mato Grosso',
+'MS': 'Mato Grosso do Sul',
+'MG': 'Minas Gerais',
+'PA': 'Pará',
+'PB': 'Paraíba',
+'PR': 'Paraná',
+'PE': 'Pernambuco',
+'PI': 'Piauí',
+'RJ': 'Rio de Janeiro',
+'RN': 'Rio Grande do Norte',
+'RS': 'Rio Grande do Sul',
+'RO': 'Rondônia',
+'RR': 'Roraima',
+'SC': 'Santa Catarina',
+'SP': 'São Paulo',
+'SE': 'Sergipe',
+'TO': 'Tocantins'
+}
